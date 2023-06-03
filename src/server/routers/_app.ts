d9a9bf1178e4f9a436408af44aebb57017e2d33b
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-06-03 21:44:50 +0530

    appRouter_updated

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,21 +1,52 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
+import { instance, provider } from "@/utils/instance";
+import { Transaction } from "ethers";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
   issue: procedure
     .input(
       z.object({
-        id: z.string(),
+        id: z.number().nullable(),
         name: z.string(),
         course: z.string(),
         grade: z.string(),
         date: z.string(),
       })
     )
-    .mutation((req) => {
-      return {
-        message: `Certificate for ID: ${req.input.id} is issued`,
-      };
+    .mutation(async (req) => {
+      const trx: Transaction = await instance.issue(
+        req.input.id,
+        req.input.name,
+        req.input.course,
+        req.input.grade,
+        req.input.date
+      );
+      console.log(trx);
+      const receipt = await provider.getTransactionReceipt(trx.hash!);
+      console.log(receipt);
+      if (!!receipt.status) {
+        return {
+          message: `Certificate for ID: ${req.input.id} is issued`,
+        };
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Transaction has failed to commit",
+        });
+      }
+    }),
+
+  fetch: procedure
+    .input(
+      z.object({
+        id: z.number().nullish(),
+      })
+      .nullish()
+    )
+    .query((req) => {
+      return req.input?.id;
     }),
 });
 
