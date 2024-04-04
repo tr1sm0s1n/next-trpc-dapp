fc830586d85ef69873cd01d933c76fda085ece39
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-04-04 15:10:55 +0530

    ⚡️ | revamped hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,16 +2,12 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  // defaultNetwork: "local",
-  solidity: "0.8.18",
+  solidity: "0.8.24",
   networks: {
     local: {
       url: "http://127.0.0.1:8545",
     },
   },
-  paths: {
-    artifacts: "../src/utils"
-  }
 };
 
 export default config;
---
+++ test/Cert.ts
@@ -0,0 +1,51 @@
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import chai from 'chai';
+import hre from 'hardhat';
+
+const { expect } = chai;
+
+describe('Cert', function () {
+  async function deployCertFixture() {
+    const [admin, other] = await hre.ethers.getSigners();
+
+    const Cert = await hre.ethers.getContractFactory('Cert');
+    const cert = await Cert.deploy();
+
+    return { cert, admin, other };
+  }
+
+  it('Should set the right admin', async function () {
+    const { cert, admin } = await loadFixture(deployCertFixture);
+
+    expect(cert.deploymentTransaction()?.from).to.equal(admin.address);
+  });
+
+  it('Should issue the certificate', async function () {
+    const { cert } = await loadFixture(deployCertFixture);
+
+    await expect(cert.issue(720, 'Cabarnet', 'MBCC', 'S', '04-04-2024'))
+      .to.emit(cert, 'Issued')
+      .withArgs(720, 'MBCC', 'S');
+  });
+
+  it('Should read the certificate', async function () {
+    const { cert } = await loadFixture(deployCertFixture);
+
+    await cert.issue(720, 'Cabarnet', 'MBCC', 'S', '04-04-2024');
+
+    const certificate = await cert.Certificates(720);    
+
+    expect(certificate[0]).to.equal('Cabarnet');
+    expect(certificate[1]).to.equal('MBCC');
+    expect(certificate[2]).to.equal('S');
+    expect(certificate[3]).to.equal('04-04-2024');
+  });
+
+  it('Should revert the issuing', async function () {
+    const { cert, other } = await loadFixture(deployCertFixture);
+
+    await expect(
+      cert.connect(other).issue(690, 'Enfer', 'MBCC', 'S', '04-04-2024')
+    ).to.be.revertedWith('Access Denied');
+  });
+});
