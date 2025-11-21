e9de37c71fadbebc1372f5d71c7c23f3adb3e6cb
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-11-21 11:44:10 +0530

    💥 | update Hardhat config and TS config
    ---------
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,43 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
+import hardhatToolboxViemPlugin from '@nomicfoundation/hardhat-toolbox-viem'
+import { configVariable, defineConfig } from 'hardhat/config'
 
-const config: HardhatUserConfig = {
-  solidity: '0.8.24',
-  networks: {
-    local: {
-      url: 'http://127.0.0.1:8545',
+export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: '0.8.28',
+      },
+      production: {
+        version: '0.8.28',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     },
   },
-}
-
-export default config
+  networks: {
+    hardhatMainnet: {
+      type: 'edr-simulated',
+      chainType: 'l1',
+    },
+    hardhatOp: {
+      type: 'edr-simulated',
+      chainType: 'op',
+    },
+    local: {
+      type: 'http',
+      chainType: 'generic',
+      url: 'http://127.0.0.1:8545',
+    },
+    sepolia: {
+      type: 'http',
+      chainType: 'l1',
+      url: configVariable('SEPOLIA_RPC_URL'),
+      accounts: [configVariable('SEPOLIA_PRIVATE_KEY')],
+    },
+  },
+})
